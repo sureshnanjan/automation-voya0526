e165c036cde0f4b3fda79608c5c0f58e3f1182c9
prince kumar <princeparashar.muz@gmail.com>
2026-06-02 03:55:05 +0000

    added color

--- learning/without-color.js
+++ learning/without-color.js
@@ -9,3 +9,4 @@ console.log(color("Error", "red+blink"));
 
 // Print the word "Success" in bold green, followed by a message
 console.log(color("Success", "green+bold"), "This is to demo external Packages");
+console.log(color("Prince", "yellow"), "My self prince");

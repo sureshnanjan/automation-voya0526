03fe90cdcb674d3a728b4d91f7036d140302b3d5
NAGARAJ-123 <niitagi@gmail.com>
2026-06-03 08:44:34 +0000

    Added new changes

--- learning/pet_store_model.js
+++ learning/pet_store_model.js
@@ -3,12 +3,28 @@
 
 const api_success = {code: 200, type: "success", message: "successful operation"};
 
-const fish_category = {id:1,name:"fish"};
-const dog_category = {id:2,name:"dog"};
-const cat_category = {id:3,name:"cat"};
+// const fish_category = {id:1,name:"fish"};
+// const dog_category = {id:2,name:"dog"};
+// const cat_category = {id:3,name:"cat"};
 
-const Angelfish = {id: 100,category	:fish_category,
-name: "angel_fish",
-photoUrls:	["fish1.jpg", "fish2.jpg    "],
-tags:["pet", "aquatic"],
+
+// const Angelfish = {id: 100,category	:fish_category,
+// name: "angel_fish",
+// photoUrls:	["fish1.jpg", "fish2.jpg    "],
+// tags:["pet", "aquatic"],
+// status: "available"}
+
+// Console.log(Angelfish);
+
+
+const bird_category = {id:4,name:"bird"};
+
+const AmazonParrot= {id: 102,category :bird_category,
+name: "bird",
+photoUrls:	["bird1.jpg", "bird2.jpg"],
+tags:["pet", "exotic"],
 status: "available"}
+
+Console.log(AmazonParrot);
+
+

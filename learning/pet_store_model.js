3f2413293e1cd0529278010baa683c24edb923b0
Rahul Verma <rahulkumarverma1907@gmail.com>
2026-06-03 07:14:00 +0000

    Pet store added amazon parret

--- learning/pet_store_model.js
+++ learning/pet_store_model.js
@@ -7,8 +7,15 @@ const fish_category = {id:1,name:"fish"};
 const dog_category = {id:2,name:"dog"};
 const cat_category = {id:3,name:"cat"};
 
+const birds_category = {id:4,name:"birds"};
+
 const Angelfish = {id: 100,category	:fish_category,
 name: "angel_fish",
 photoUrls:	["fish1.jpg", "fish2.jpg    "],
 tags:["pet", "aquatic"],
 status: "available"}
+
+const AmazonParrot = {id: 101, category: birds_category, 
+    name: "amazon_parrot", photoUrls: ["bird1.jpg", "bird2.jpg"],
+     tags: ["pet", "exotic"], status: "available"};
+console.log(AmazonParrot);

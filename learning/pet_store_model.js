029f5992506aa1e7eed84daf01862eefef9fbb41
Rahul Verma <rahulkumarverma1907@gmail.com>
2026-06-03 07:14:00 +0000

    Pet store added amazon parret

--- learning/pet_store_model.js
+++ learning/pet_store_model.js
@@ -2,18 +2,20 @@
 // https://petstore.swagger.io/
 
 const api_success = {code: 200, type: "success", message: "successful operation"};
-const failure = {code: 200, type: "success", message: "successful operation",notvalid: "This is not a valid attribute"};
 
 const fish_category = {id:1,name:"fish"};
 const dog_category = {id:2,name:"dog"};
 const cat_category = {id:3,name:"cat"};
 
-fish_category.name;
-fish_category.notexisting; // undefined
+const birds_category = {id:4,name:"birds"};
 
 const Angelfish = {id: 100,category	:fish_category,
 name: "angel_fish",
 photoUrls:	["fish1.jpg", "fish2.jpg    "],
 tags:["pet", "aquatic"],
 status: "available"}
-console.log(Angelfish);
+
+const AmazonParrot = {id: 101, category: birds_category, 
+    name: "amazon_parrot", photoUrls: ["bird1.jpg", "bird2.jpg"],
+     tags: ["pet", "exotic"], status: "available"};
+console.log(AmazonParrot);

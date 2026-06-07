7b66f3d9c1830bf2d30cfc33a667e048ec113be6
dileep-hl <dileep.288@gmail.com>
2026-06-07 12:40:53 +0000

    Done some changes in pet_store_model.js

--- learning/pet_store_model.js
+++ learning/pet_store_model.js
@@ -17,3 +17,23 @@ photoUrls:	["fish1.jpg", "fish2.jpg    "],
 tags:["pet", "aquatic"],
 status: "available"}
 console.log(Angelfish);
+
+const order1 = {id: 100, petId: 1, quantity: 2, shipDate: "2024-06-30T12:00:00.000Z", status: "placed", complete: true};
+console.log(order1);
+const order2 = {id: 101, petId: 2, quantity: 1, shipDate: "2024-07-01T15:30:00.000Z", status: "approved", complete: false};
+console.log(order2);
+const order3 = {id: 102, petId: 3, quantity: 3, shipDate: "2024-07-02T10:00:00.000Z", status: "delivered", complete: true};
+console.log(order3);
+
+const user1 = {id: 101, username: "dileep_hl", firstName: "Dileep", lastName: "HL", email: "dileep.hl@example.com"};
+console.log(user1);
+
+const bird_category = {id:4,name:"bird"};
+const parrot = {id: 103, category: bird_category, name: "parrot", photoUrls: ["parrot1.jpg"], tags: ["pet", "avian"], status: "available"};
+const crow = {id: 104, category: bird_category, name: "crow", photoUrls: ["crow1.jpg"], tags: ["wild", "avian"], status: "unavailable"};
+console.log(parrot);
+console.log([crow]);
+
+
+const tags = [{id: 1, name: "pet"}, {id: 2, name: "aquatic"}, {id: 3, name: "avian"},{id: 4, name: "wild"}, {id: 5, name: "mammal"}];
+console.log(tags);

3b67c0494e1dffc879e5ff4aa9cb20c3b80c5b65
Rahul Verma <rahulkumarverma1907@gmail.com>
2026-06-10 07:34:29 +0000

    change

--- learning/pet_store_model.js
+++ learning/pet_store_model.js
@@ -8,15 +8,12 @@ const fish_category = {id:1,name:"fish"};
 const dog_category = {id:2,name:"dog"};
 const cat_category = {id:3,name:"cat"};
 
-const birds_category = {id:4,name:"birds"};
+fish_category.name;
+fish_category.notexisting; // undefined
 
 const Angelfish = {id: 100,category	:fish_category,
 name: "angel_fish",
 photoUrls:	["fish1.jpg", "fish2.jpg    "],
 tags:["pet", "aquatic"],
 status: "available"}
-
-const AmazonParrot = {id: 101, category: birds_category, 
-    name: "amazon_parrot", photoUrls: ["bird1.jpg", "bird2.jpg"],
-     tags: ["pet", "exotic"], status: "available"};
-console.log(AmazonParrot);
+console.log(Angelfish);

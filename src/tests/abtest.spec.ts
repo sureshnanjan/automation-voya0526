6fd3dae2084180167bc4a50d29c4661e82a81ae2
sureshnanjan <sureshnanjan@outlook.com>
2026-06-26 13:53:34 +0530

    Clean Architecture Dependency Break

--- src/tests/abtest.spec.ts
+++ src/tests/abtest.spec.ts
@@ -1,8 +1,12 @@
 import {test,expect,} from "@playwright/test";
-import { ABTestingOperations } from "../operations/abtesting_operations.js";
-test("Title is OK",()=>{
+import type { ABTestingOperations } from "../operations/abtesting_operations.js";
+import type { HomePageOperation } from "../operations/home_page_operations.js";
+import { HomePage } from "../implementation/home_page.js";
+
+test("Title is OK",async ({page})=>{
     // Title is either AB Test Control | AB Test Variation 1
-    const abtestPage:ABTestingOperations;
+    const herokuapp:HomePageOperation = HomePage.Create(page)
+    const resultPage:ABTestingOperations = herokuapp.goToExample("A/B Testing")
     const expected_result = ["AB Test Control", "AB Test Variation 1"]
     const actual = abtestPage.getTitle();
     expect(expected_result).toContain(actual)
@@ -15,5 +19,14 @@ test("Title is will always be NO ABTest if AB Testing disabled",()=>{
     const expected_result = "No AB Test"
     const actual = abtestPage.getTitle();
     expect(expected_result).toEqual(actual)
+
+});
+test("Title be Eother of the 2 options when AB Testing is enabled",()=>{
+    // Title is either AB Test Control | AB Test Variation 1
+    const abtestPage:ABTestingOperations;
+    abtestPage.enableABTesting();
+    const expected_result = ["AB Test Variation1","AB Testcontrol"]
+    const actual = abtestPage.getTitle();
+    expect(expected_result).toContain(actual)
 
 });
